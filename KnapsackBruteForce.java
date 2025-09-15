1e8337ab05727e2d133dadf50fb12f726e50f25e
Iago Luz <88354279+iagoluz01@users.noreply.github.com>
2025-09-15 23:14:03 +0000

    adding fix

--- KnapsackBruteForce.java
+++ KnapsackBruteForce.java
@@ -1,5 +1,6 @@
 public class KnapsackBruteForce {
     private static int iterations = 0;
+    private static boolean[] bestSelection;
     
     static class Item {
         int weight;
@@ -11,24 +12,37 @@ public class KnapsackBruteForce {
         }
     }
 
-    public static int knapsackBruteForce(Item[] items, int capacity, int n) {
+    public static int knapsackBruteForce(Item[] items, int capacity, int n, boolean[] selected) {
         iterations++;
         
         // Base case
-        if (n == 0 || capacity == 0)
+        if (n == 0 || capacity == 0) {
             return 0;
-            
+        }
+        
         // If weight of item is more than capacity, skip it
-        if (items[n-1].weight > capacity)
-            return knapsackBruteForce(items, capacity, n-1);
-            
-        // Return maximum of:
-        // 1. Item n-1 included
-        // 2. Item n-1 not included
-        return Math.max(
-            items[n-1].value + knapsackBruteForce(items, capacity - items[n-1].weight, n-1),
-            knapsackBruteForce(items, capacity, n-1)
-        );
+        if (items[n-1].weight > capacity) {
+            boolean[] newSelected = selected.clone();
+            return knapsackBruteForce(items, capacity, n-1, newSelected);
+        }
+        
+        boolean[] includedItems = selected.clone();
+        includedItems[n-1] = true;
+        
+        boolean[] excludedItems = selected.clone();
+        excludedItems[n-1] = false;
+        
+        int includeItem = items[n-1].value + 
+                         knapsackBruteForce(items, capacity - items[n-1].weight, n-1, includedItems);
+        int excludeItem = knapsackBruteForce(items, capacity, n-1, excludedItems);
+        
+        if (includeItem > excludeItem) {
+            bestSelection = includedItems;
+            return includeItem;
+        } else {
+            bestSelection = excludedItems;
+            return excludeItem;
+        }
     }
 
     public static void main(String[] args) {
@@ -42,9 +56,25 @@ public class KnapsackBruteForce {
         
         int capacity = 10;
         iterations = 0;
+        bestSelection = new boolean[items.length];
         
-        int maxValue = knapsackBruteForce(items, capacity, items.length);
+        int maxValue = knapsackBruteForce(items, capacity, items.length, new boolean[items.length]);
+        
+        System.out.println("Knapsack Solution (Brute Force):");
+        System.out.println("--------------------------------");
+        System.out.println("Capacity: " + capacity);
         System.out.println("Maximum value: " + maxValue);
         System.out.println("Number of iterations: " + iterations);
+        System.out.println("\nSelected items:");
+        
+        int totalWeight = 0;
+        for (int i = 0; i < items.length; i++) {
+            if (bestSelection[i]) {
+                System.out.printf("Item %d: (weight=%d, value=%d)\n", 
+                                i+1, items[i].weight, items[i].value);
+                totalWeight += items[i].weight;
+            }
+        }
+        System.out.println("\nTotal weight: " + totalWeight);
     }
 }
